b16c89272b4a2bc73797473dc6bda4e6f1bf015e
Daniel Lindberg <dajoh10@gmail.com>
2014-07-02 15:37:01 +0200

    Fixed incorrect specs.

--- include/sqlmux.hrl
+++ include/sqlmux.hrl
@@ -1,10 +1,10 @@
 %% @doc A 'where condition' is either a pair on the form {Column, Value} corresponding to an equals
 %% condition or a string/binary/iolist of SQL code (an SQL expression). In the former, Value is an
 %% unquoted value.
--type where_cond() :: {term(), term()} | iolist().
+-type where_cond() :: {term(), term()} | iodata().
 
 %% @doc An SQL SELECT query. See make_select/4 for documentation.
--record(sqlquery, {select      :: [iolist()],
-                   from   = [] :: iolist(),
-                   where  = [] :: where_cond(),
-                   other  = [] :: iolist()}).
+-record(sqlquery, {select      :: iodata(),
+                   from   = [] :: iodata(),
+                   where  = [] :: [where_cond()],
+                   other  = [] :: iodata()}).
